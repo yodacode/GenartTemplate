f029cce841fd3a906aaa60fa39230fd724ee2568
unknown <Etienne@Etienne-PC_Port.(none)>
2013-02-27 17:37:01 +0100

    formulaire detail

--- detail.php
+++ detail.php
@@ -25,26 +25,26 @@
             </div>
         </div>
         <div class="span4">
-            <form id="form-art">
-                <div class="span3">
-                    Value 1 : <input type="text" name="val1">
-                </div>
-                <div class="span3">
-                    Value 2 : <input type="text" name="val2">
-                </div>
-                <div class="span3">
-                    Value 3 : <input type="text" name="val3">
-                </div>
-                <div class="span3">
-                    Value 4 : <input type="text" name="val4">
-                </div>
-                <div class="span3">
-                    <a class="button grey"value="Reset">Reset</a> 
-                    <input class="button red noborder" type="submit" value="Valider">
-                </div>
-
-            </form>
-
+            <div class="myform">
+                <form id="form-art" class="form-horizontal">
+                    <div class="control-group">
+                        <label for="val1">Value 1 : </label><input type="text" id="val1" name="val1">
+                    </div>
+                    <div class="control-group">
+                        <label for="val2">Value 2 : </label><input type="text" id="val2" name="val2">
+                    </div>
+                    <div class="control-group">
+                        <label for="val3">Value 3 : </label><input type="text" id="val3" name="val3">
+                    </div>
+                    <div class="control-group">
+                        <label for="val4">Value 4 : </label><input type="text" id="val4" name="val4">
+                    </div>
+                    <div class="control-group">
+                        <a class="button grey" value="Reset">Reset</a> 
+                        <input class="button red noborder" type="submit" value="Valider">
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
 </section>
