78727bfca1c31da4f265b6564edb347a46c0364d
Benjamin de Vaublanc <benjamin.devaublanc@gmail.com>
2013-02-27 16:06:26 +0100

    add all files

---
+++ listing.php
@@ -0,0 +1,109 @@
+﻿<?php include('header.php'); ?>
+
+
+  <header class="header">
+    <section class="container">
+      <h3>Gallerie categorie : lorem ipsum</h3>
+
+    </section>
+    <button class="btn btn-primary visible-phone">sdqdqsd</button>
+  </header>
+
+  <section class="content">
+    <section class="container-fluid">
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture2.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture2.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture2.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture3.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture2.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture2.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+      <article class="span4 article-thumb">
+        <figure>
+          <img src="img/picture2.jpg"/>
+        </figure>
+        <h3>Titre de l'oeuvre</h3>
+        <p>
+          Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+          Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+        </p>
+        <a href="#" class="button green">en savoir plus</a>
+      </article>
+    </section>
+    
+    <div class="spacer"></div>
+
+   
+  </section><!--end content-->
+
+<?php include('footer.php');?>
