98d560d4f3368b4af6ea4e790ca5e64a6271386e
unknown <Etienne@Etienne-PC_Port.(none)>
2013-02-27 16:14:10 +0100

    add detail

---
+++ detail.php
@@ -0,0 +1,40 @@
+<?php include('header.php'); ?>
+
+<header class="header">
+    <section class="container">
+        <h1>Information sur l'oeuvre : MegaSuper</h1>
+    </section>
+</header>
+
+<section class="content">
+    <section class="container-fluid">
+        <div class="span8">
+            <div id="info-oeuvre">
+
+                <h3>Titre de l'oeuvre</h3>
+                <p>
+                    Processing is an open source programming language and environment for people who want to create images, animations, and interactions. 
+                    Initially developed to serve as a software sketchbook and to teach there are tens of thousands of students.
+                </p>
+            </div>
+        </div>
+        
+        <div class="span8">
+            <div id="appercu">
+                
+            </div>
+        </div>
+        <div class="span4">
+            <form>
+                <input type="text" name="val1">
+                <input type="text" name="val2">
+                <input type="text" name="val3">
+                <input type="text" name="val4">
+                <input type="submit" value="Valider">
+            </form>
+
+        </div>
+    </section>
+</section>
+
+<?php include('footer.php'); ?>
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <link href="css/bootstrap/bootstrap-responsive.css" rel="stylesheet"/>
     <link href='http://fonts.googleapis.com/css?family=Monda|Julius+Sans+One' rel='stylesheet' type='text/css'/>
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/sup.css" rel="stylesheet">
     
     <!--<script src="http://code.jquery.com/jquery-latest.js"></script>-->
     <script src="js/jquery.js"></script>
