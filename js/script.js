350d48e74d2e96328e89ee511d19b901def891bf
Benjamin de Vaublanc <benjamin.devaublanc@gmail.com>
2013-02-28 15:08:47 +0100

    add animation

--- js/script.js
+++ js/script.js
@@ -6,7 +6,51 @@ var init = function(){
       e.preventDefault();
       $(this).tab('show');
     });
+
+
+    var $article = $('.article-thumb');
+
+    $article.on('click', function(){
+        var linkDetail = $(this).find('a').attr('href');
+        window.location = linkDetail;
+    });
+
+    var getGoodHeight = function($heightFigure, $heightImg){
+        var $height = null;
+        if ($heightFigure < $heightImg){
+            $height = $heightFigure;
+        } else {
+            $height = $heightImg;
+        }
+
+        return $height;
+    };
+
+    $article.on('mouseenter', function(){
         
+        var $height = getGoodHeight($(this).find('figure').height(), $(this).find('img').height());
+        var $elem = $(this).find('.pict-descr');
+
+        $elem.css({'height' : $height })
+        $elem.show();
+        $elem.stop().animate({
+            opacity : 0.8
+        },500);
+    
+    });
+
+    $article.on('mouseleave', function(){
+        
+        var $height = getGoodHeight($(this).find('figure').height(), $(this).find('img').height());
+        var $elem = $(this).find('.pict-descr');
+
+        $elem.css({'height' : $height})
+        $elem.stop().animate({
+            opacity : 0
+        },500, function(){
+            $elem.hide();
+        });
+    });
         
 
 }
