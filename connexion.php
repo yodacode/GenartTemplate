bbc18b577beeea35a24fb6974fdbaa35578222b4
Moufasa <moufasa.contact@gmail.com>
2013-02-28 15:32:39 +0100

    validation contact v1

--- connexion.php
+++ connexion.php
@@ -13,21 +13,26 @@
 				<h2>Déjà membre</h2>
 			</section>
 
-			<form class="form-horizontal">
-				<div class="control-group">
+			<form name="authentification" class="form-horizontal">
+				<div id="login-control-group" class="control-group">
 					<label for="login" class="control-label">Identifiant : </label>
-					<input type="text" id="login" name="login" placeholder="identifiant"/>
+					<div class="controls">
+						<input data-error-selector="login-message" type="text" id="login" name="login" placeholder="identifiant"/>
+						<span class="help-inline" style="display:none;" id="login-message"> Veuillez utiliser votre E-mail pour vous authentifier. </span>
+					</div>
 				</div>
-				<div class="control-group">
+				<div id="password-control-group" class="control-group">
 					<label for="password" class="control-label">Mot de passe : </label>
-					<input type="password" id="password"  name="password" placeholder="Mot de passe"/>
+					<div class="controls">
+						<input data-error-selector="password-message" type="password" id="password"  name="password" placeholder="Mot de passe"/>
+						<span class="help-inline" style="display:none;" id="password-message"> Ce champ est obligatoire. </span>
+					</div>
 				</div>
 				<input type="submit" class="button red noborder pull-right" value="Connexion"/>
 			</form>
 				
 		</section>
 		<section class="span5">
-
 			<section class="page-header">
 				<h2>Inscription</h2>
 			</section>
@@ -41,57 +46,157 @@
 			</ul>
 			<section class="tab-content">
 				
-				<form class="form-horizontal tab-pane fade active in" id="artist">
-					<div class="control-group">
-						<label for="nameArtist" class="control-label">Nom* : </label>
-						<input type="text" id="nameArtist"  name="nameArtist" placeholder="Votre nom"/>
+				<form name="form-artist" class="form-horizontal tab-pane fade active in" id="artist">
+					<div id="nameArtist-control-group" class="control-group">
+						<label for="nameArtist" class="control-label">Nom  * : </label>
+						<div class="controls">
+							<input data-error-selector="nameArtist-message" type="text" id="nameArtist"  name="nameArtist" placeholder="Votre nom"/>
+							<span class="help-inline" style="display:none;" id="nameArtist-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
-						<label for="firstnameArtist" class="control-label">Prénom* : </label>
-						<input type="text" id="firstnameArtist"  name="firstnameArtist" placeholder="Prénom"/>
+					<div id="firstnameArtist-control-group" class="control-group">
+						<label for="firstnameArtist" class="control-label">Prénom  * : </label>
+						<div class="controls">
+							<input data-error-selector="firstnameArtist-message" type="text" id="firstnameArtist"  name="firstnameArtist" placeholder="Prénom"/>
+							<span class="help-inline" style="display:none;" id="firstnameArtist-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
-						<label for="mailArtist" class="control-label">E-mail* : </label>
-						<input type="text" id="mailArtist"  name="mailArtist" placeholder="vote-mail@mail.com"/>
+					<div id="mailArtist-control-group" class="control-group">
+						<label for="mailArtist" class="control-label">E-mail  * : </label>
+						<div class="controls">
+							<input data-error-selector="mailArtist-message" type="text" id="mailArtist"  name="mailArtist" placeholder="vote-mail@mail.com"/>
+							<span class="help-inline" style="display:none;" id="mailArtist-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
-						<label for="passwordArtist" class="control-label">Mot de passe* : </label>
-						<input type="password" id="passwordArtist"  name="passwordArtist" placeholder="Mot de passe"/>
+					<div id="passwordArtist-control-group" class="control-group">
+						<label for="passwordArtist" class="control-label">Mot de passe * : </label>
+						<div class="controls">
+							<input data-error-selector="passwordArtist-message" type="password" id="passwordArtist"  name="passwordArtist" placeholder="Mot de passe"/>
+							<span class="help-inline" style="display:none;" id="passwordArtist-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
-						<label for="passwordArtistConfirm" class="control-label">Confirmation* : </label>
-						<input type="password" id="passwordArtistConfirm"  name="passwordArtistConfirm" placeholder="Confirmation"/>
+					<div id="passwordArtistConfirm-control-group" class="control-group">
+						<label for="passwordArtistConfirm" class="control-label">Confirmation  * : </label>
+						<div class="controls">
+							<input data-error-selector="passwordArtistConfirm-message" type="password" id="passwordArtistConfirm"  name="passwordArtistConfirm" placeholder="Confirmation"/>
+							<span class="help-inline" style="display:none;" id="passwordArtistConfirm-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
-						<label for="phoneArtist" class="control-label">Téléphone* : </label>
-						<input type="text" id="phoneArtist"  name="phoneArtist" placeholder="Téléphone"/>
+					<div id="phoneArtist-control-group" class="control-group">
+						<label for="phoneArtist" class="control-label">Téléphone  * : </label>
+						<div class="controls">
+							<input data-error-selector="phoneArtist-message" type="text" id="phoneArtist"  name="phoneArtist" placeholder="Téléphone"/>
+							<span class="help-inline" style="display:none;" id="phoneArtist-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
+					<div id="websiteArtist-control-group" class="control-group">
 						<label for="websiteArtist" class="control-label">Site web : </label>
-						<input type="text" id="websiteArtist"  name="websiteArtist" placeholder="www.votre-site.com"/>
+						<div class="controls">
+							<input data-error-selector="websiteArtist-message" type="text" id="websiteArtist"  name="websiteArtist" placeholder="www.votre-site.com"/>
+							<span class="help-inline" style="display:none;" id="websiteArtist-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
 					<input type="submit" class="button red noborder pull-right" value="Inscription"/>
 				</form>
 
 				<form class="form-horizontal tab-pane fade " id="customer">
-					<div class="control-group">
-						<label for="mailArtist" class="control-label">E-mail* : </label>
-						<input type="text" id="mailArtist"  name="mailArtist" placeholder="vote-mail@mail.com"/>
+					<div id="-control-group" class="control-group">
+						<label for="mailArtist" class="control-label">E-mail  * : </label>
+						<div class="controls">
+							<input data-error-selector="-message" type="text" id="mailArtist"  name="mailArtist" placeholder="vote-mail@mail.com"/>
+							<span class="help-inline" style="display:none;" id="-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
-						<label for="passwordArtist" class="control-label">Mot de passe* : </label>
-						<input type="password" id="passwordArtist"  name="passwordArtist" placeholder="Mot de passe"/>
+					<div id="-control-group" class="control-group">
+						<label for="passwordArtist" class="control-label">Mot de passe  * : </label>
+						<div class="controls">
+							<input data-error-selector="-message" type="password" id="passwordArtist"  name="passwordArtist" placeholder="Mot de passe"/>
+							<span class="help-inline" style="display:none;" id="-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
-					<div class="control-group">
-						<label for="passwordArtistConfirm" class="control-label">Confirmation* : </label>
-						<input type="password" id="passwordArtistConfirm"  name="passwordArtistConfirm" placeholder="Confirmation"/>
+					<div id="-control-group" class="control-group">
+						<label for="passwordArtistConfirm" class="control-label">Confirmation  * : </label>
+						<div class="controls">
+							<input data-error-selector="-message" type="password" id="passwordArtistConfirm"  name="passwordArtistConfirm" placeholder="Confirmation"/>
+							<span class="help-inline" style="display:none;" id="-message"> lorem ipsum para bellum </span>
+						</div>
 					</div>
 					<input type="submit" class="button red noborder pull-right" value="Inscription"/>
 				</form>
-
 			</section>
 		</section>
 	</section>
 </section><!--end content-->
+<script type="text/javascript">
+	$(function() {
+            var validator = new FormValidator("authentification",
+                [
+                    {
+                        name: "login", //name du champ.
+                        rules: 'required|valid_email' //choix du validator chainé par des pipes
+                    },
+                    {
+                        name: "password",
+                        rules: 'required'
+                    }
+                ],function(errors, event){
+                if (errors.length > 0) {
+                    for(var index in errors){
+                        var idElement = errors[index].id;
+                        var helperSelector = $("#"+idElement).attr("data-error-selector");
+                        $("#"+helperSelector).show();
+                        var groupSelector = "#"+errors[index].name+"-control-group";
+                        $(groupSelector).addClass("error");
+                    }
+                }
+             });
+// nameArtist
+            var validator = new FormValidator("form-artist",
+                [
+                    {
+                        name: "mailArtist", //name du champ.
+                        rules: 'required|valid_email' //choix du validator chainé par des pipes
+                    },
+                    {
+                        name: "passwordArtist",
+                        rules: 'required'
+                    },
+                    {
+                        name: "nameArtist",
+                        rules: 'required'
+                    },
+                    {
+                        name: "firstnameArtist",
+                        rules: 'required'
+                    },
+                    {
+                        name: "passwordArtistConfirm",
+                        rules: 'required|matches[passwordArtist]'
+                    },
+                    {
+                        name: "passwordArtistConfirm",
+                        rules: 'required|matches[passwordArtist]'
+                    },
+                    {
+                        name: "phoneArtist",
+                        rules: 'required|max_length[10]|min_length[10]'
+                    },
+                    {
+                        name: "passwordArtistConfirm",
+                        rules: 'required|matches[passwordArtist]'
+                    }
 
+                ],function(errors, event){
+                if (errors.length > 0) {
+                    for(var index in errors){
+                        var idElement = errors[index].id;
+                        var helperSelector = $("#"+idElement).attr("data-error-selector");
+                        $("#"+helperSelector).show();
+                        var groupSelector = "#"+errors[index].name+"-control-group";
+                        $(groupSelector).addClass("error");
+                    }
+                }
+             });		
+	});
+</script>
 <?php include('footer.php');?>
