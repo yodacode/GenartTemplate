15f9be97ebd1fb9d74d3921bfefeb0becfbffddc
Moufasa <moufasa.contact@gmail.com>
2013-02-28 12:15:31 +0100

    rename + add charset

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <title>Template Base</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
+    <meta charset="utf-8">
     <!-- Bootstrap -->
     <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen"/>
     <link href="css/bootstrap/bootstrap-responsive.css" rel="stylesheet"/>
