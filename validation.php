1ff6e71882d6f0b5f674a68c22df8558af04d5fe
etienne <datcharrye@gmail.com>
2013-03-01 10:52:44 +0100

    add 404png + validation.php

---
+++ validation.php
@@ -0,0 +1,22 @@
+<?php include('header.php'); ?>
+
+<header class="header">
+    <section class="container">
+        <h3>Inscription validée</h3>
+    </section>
+</header>
+
+<section class="content">
+    <div class="container auto">
+        <h2>Votre inscription à bien été validée</h2>
+        <p>vous pouvez des maintenant accéder aux fonctionnalités avancées du site</p>
+        <p>
+            <a class="button green" href="index.php">
+                retour à l'accueil
+            </a>
+        </p>
+    </div>
+
+</section><!--end content-->
+
+<?php include('footer.php'); ?>
