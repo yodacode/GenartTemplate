a91c76e04d7d14461b53a2025bdb81d27f8baca7
yodacode <benjamin.devaublanc@gmail.com>
2013-05-23 14:53:12 +0200

    create detail artist and history command

--- artist.php
+++ artist.php
@@ -7,7 +7,23 @@
   </header>
 
   <section class="content">
-   
+   <div class="container">
+   		<div class="pull-left span2">
+			<img src="img/artist.jpg" class="img-circle img-polaroid img-detail-artist">
+		</div>
+		<div class="pull-left span8" style="margin-left:60px;">
+			<div class="page-header">
+				<h2>Nom de l'artiste</h2>
+			</div>
+			<div class="description-artist">
+				<p>
+					Nemo quaeso miretur, si post exsudatos labores itinerum longos congestosque adfatim commeatus fiducia vestri ductante barbaricos pagos adventans velut mutato repente consilio ad placidiora deverti<br/>
+					Nemo quaeso miretur, si post exsudatos labores itinerum longos congestosque adfatim commeatus fiducia vestri ductante barbaricos pagos adventans velut mutato repente consilio ad placidiora deverti<br/><br/>
+					Nemo quaeso miretur, si post exsudatos labores itinerum longos congestosque adfatim commeatus fiducia vestri ductante barbaricos pagos adventans velut mutato repente consilio ad placidiora deverti
+				</p>
+			</div>
+		</div>
+   </div>
   </section><!--end content-->
 
  <?php include('footer.php');?>
